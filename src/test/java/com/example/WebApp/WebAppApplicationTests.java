5999545e9cae5fdecdae006cfedf8445f04b8b85
DavidEsdrs <carpina1983@gmail.com>
2025-08-04 02:10:37 -0300

    fix test

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -1,14 +1,14 @@
 package com.example.WebApp;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.Matchers.containsString;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebAppApplicationTests {
@@ -40,7 +40,7 @@ class WebAppApplicationTests {
 	}
 	
 	@Test
-	public void shouldReturnOutro() throws Exception {
+	public void shouldReturnCode() throws Exception {
 		this.mockMvc.perform(get("/code-endpoint"))
 				.andDo(print()).andExpect(status().isOk())
 				.andExpect(content()
